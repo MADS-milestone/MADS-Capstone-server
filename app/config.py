6dd8f3e1f28ed3828f2a5d67144d893ceee13234
dmitry.danilov <dmitry.danilov@gmail.com>
2024-04-03 18:36:58 +0300

    embedding vector length increased to 3072

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     connection_str: str = "postgresql://postgres:postgres@localhost:5432/vector_db"
     index_table: str = "clinical_rag_hybrid_search"
-    embed_dim: int = 1536
+    embed_dim: int = 3072
 
 
 config = Settings()
